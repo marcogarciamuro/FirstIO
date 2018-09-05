bc8dd80da886c0f24d7c83f8f2851c18bb00683d
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-09-05 16:31:56 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,12 @@
+// Author: Marco Garcia Muro
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout <<
+           "What's your first name?" << std::endl;
     std::cin >> input;
-    std::cout << "Hello, " << input << std::endl;
+    std::cout << "Hi, my name is Marco. What's yours?, " << input << std::endl;
     return 0;
 }
